565a6583849466dbd69e7d33a8471ac03d2eaa32
ranjit <ranjithkumar2010a@live.com>
2017-09-06 13:04:48 +0530

    Removed basic Storyboard and configured with xib and window property

---
+++ InstagramStories/Controllers/IGHomeController.swift
@@ -0,0 +1,19 @@
+//
+//  IGHomeController.swift
+//  InstagramStories
+//
+//  Created by Ranjith Kumar on 9/6/17.
+//  Copyright © 2017 Dash. All rights reserved.
+//
+
+import UIKit
+
+class IGHomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Home"
+    }
+
+   
+}
