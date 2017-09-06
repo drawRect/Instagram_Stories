1b0852e3cf26a5342d294bd11d99a97efa5ebcd1
ranjit <ranjithkumar2010a@live.com>
2017-09-06 13:48:03 +0530

    Add Story and Story List custom collectionView cells are added

--- InstagramStories/Controllers/IGHomeController.swift
+++ InstagramStories/Controllers/IGHomeController.swift
@@ -8,12 +8,51 @@
 
 import UIKit
 
+struct IGStory{
+    
+}
+
 class IGHomeController: UIViewController {
 
+    @IBOutlet weak var storiesCollectionView: UICollectionView! {
+        didSet {
+            storiesCollectionView.delegate = self
+            storiesCollectionView.dataSource = self
+            let storyListNib = UINib.init(nibName: IGStoryListCell.reuseIdentifier(), bundle: nil)
+            storiesCollectionView.register(storyListNib, forCellWithReuseIdentifier: IGStoryListCell.reuseIdentifier())
+            let addStoryNib = UINib.init(nibName: IGAddStoryCell.reuseIdentifier(), bundle: nil)
+            storiesCollectionView.register(addStoryNib, forCellWithReuseIdentifier: IGAddStoryCell.reuseIdentifier())
+        }
+    }
+    let stories:[IGStory] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Home"
+        self.automaticallyAdjustsScrollViewInsets = false
     }
-
    
+}
+
+extension IGHomeController:UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10 + 1 // Add Story cell
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.row == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: IGAddStoryCell.reuseIdentifier(),for: indexPath) as! IGAddStoryCell
+            return cell
+        }else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: IGStoryListCell.reuseIdentifier(),for: indexPath) as! IGStoryListCell
+            cell.profileImageView.backgroundColor = UIColor.brown
+            cell.profileNameLabel.text = "Story-\(indexPath.row+1)"
+            return cell
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return indexPath.row == 0 ? CGSize(width: 100, height: 100) : CGSize(width: 80, height: 100)
+    }
 }
---
+++ InstagramStories/IGAddStoryCell.swift
@@ -0,0 +1,21 @@
+//
+//  IGAddStoryCell.swift
+//  InstagramStories
+//
+//  Created by Ranjith Kumar on 9/6/17.
+//  Copyright © 2017 Dash. All rights reserved.
+//
+
+import UIKit
+
+class IGAddStoryCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    public class func reuseIdentifier()->String{
+        return "IGAddStoryCell"
+    }
+}
