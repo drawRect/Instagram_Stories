b561b118386d2eef22de4696147d4fcb7fcb8786
boominadhaprakash <boominadhaprakash@gmail.com>
2018-03-18 20:39:34 +0530

    Implemented MVVM architecture for StoryPreviewController

---
+++ InstagramStories/IGStoryPreviewModel.swift
@@ -0,0 +1,39 @@
+//
+//  IGStoryPreviewModel.swift
+//  InstagramStories
+//
+//  Created by Boominadha Prakash on 18/03/18.
+//  Copyright © 2018 Dash. All rights reserved.
+//
+
+import Foundation
+
+class IGStoryPreviewModel: NSObject {
+    
+    //MARK:- iVars
+    var stories: IGStories?
+    var handPickedStoryIndex: Int? //starts with(i)
+    
+    //MARK:- Init method
+    init(_ stories: IGStories, _ handPickedStoryIndex: Int) {
+        self.stories = stories
+        self.handPickedStoryIndex = handPickedStoryIndex
+    }
+    
+    //MARK:- Functions
+    func numberOfItemsInSection(_ section: Int) -> Int {
+        if let count = stories?.count {
+            return count-handPickedStoryIndex!
+        }
+        return 0
+    }
+    func cellForItemAtIndexPath(_ indexPath: IndexPath) -> IGStory? {
+        guard let handPickedIndex = handPickedStoryIndex, let count = stories?.count else {return nil}
+        let counted = handPickedIndex+indexPath.item
+        if counted < count {
+            return stories?.stories?[counted]
+        }else {
+            fatalError("Stories Index mis-matched :(")
+        }
+    }
+}
