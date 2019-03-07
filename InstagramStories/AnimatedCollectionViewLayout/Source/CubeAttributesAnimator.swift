593a8b33e1d6c5c960966c0c3da24ce11639663b
boominadhaprakash <boominadhaprakash@gmail.com>
2019-03-07 21:58:52 +0530

    Code optimisation

--- InstagramStories/AnimatedCollectionViewLayout/Source/CubeAttributesAnimator.swift
+++ InstagramStories/AnimatedCollectionViewLayout/Source/CubeAttributesAnimator.swift
@@ -41,12 +41,8 @@ public struct CubeAttributesAnimator: LayoutAttributesAnimator {
             UIView.animate(withDuration: 0.2, animations: {
                 attributes.contentView?.subviews.first?.isUserInteractionEnabled = false
                 attributes.contentView?.layer.transform = transform
-            }) { (status) in
-                if status {
-                    attributes.contentView?.subviews.first?.isUserInteractionEnabled = true
-                } else {
-                    attributes.contentView?.subviews.first?.isUserInteractionEnabled = false
-                }
+            }) {
+                attributes.contentView?.subviews.first?.isUserInteractionEnabled = $0
             }
             attributes.contentView?.keepCenterAndApplyAnchorPoint(CGPoint(x: position > 0 ? 0 : 1, y: 0.5))
         } else {
@@ -64,14 +60,9 @@ public struct CubeAttributesAnimator: LayoutAttributesAnimator {
             UIView.animate(withDuration: 0.2, animations: {
                 attributes.contentView?.subviews.first?.isUserInteractionEnabled = false
                 attributes.contentView?.layer.transform = transform
-            }) { (status) in
-                if status {
-                    attributes.contentView?.subviews.first?.isUserInteractionEnabled = true
-                } else {
-                    attributes.contentView?.subviews.first?.isUserInteractionEnabled = false
-                }
+            }) {
+                attributes.contentView?.subviews.first?.isUserInteractionEnabled = $0
             }
-            
             attributes.contentView?.keepCenterAndApplyAnchorPoint(CGPoint(x: 0.5, y: position > 0 ? 0 : 1))
         }
     }
