60440d596c3e387106a0f28b21966fde985ffc5f
boominadhaprakash <boominadhaprakash@gmail.com>
2019-05-27 08:09:00 +0530

    Modified IGAppUtitlity code

--- InstagramStories/IGAppUtility.swift
+++ InstagramStories/IGAppUtility.swift
@@ -12,7 +12,6 @@ import UIKit
 struct IGAppUtility {
     
     static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
-        
         if let delegate = UIApplication.shared.delegate as? AppDelegate {
             delegate.orientationLock = orientation
         }
@@ -20,9 +19,7 @@ struct IGAppUtility {
     
     /// OPTIONAL Added method to adjust lock and rotate to the desired orientation
     static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation:UIInterfaceOrientation) {
-        
         self.lockOrientation(orientation)
-        
         UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
         UINavigationController.attemptRotationToDeviceOrientation()
     }
