8d38710e240dac900b7896a8c6a1eb193bc7c99f
Javid Museyibli <javid.museyibli@pashabank.az>
2021-07-07 16:00:12 +0400

    1.0.10

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Stories",
     platforms: [
         // Only add support for iOS 11 and up.
-        .iOS(.v11)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
